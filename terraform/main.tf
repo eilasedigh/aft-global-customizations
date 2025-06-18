bfcad0b09e8057ff82fd6be1a2aa3dbe6b9c86a8
eilasedigh <56550611+eilasedigh@users.noreply.github.com>
2025-06-17 21:26:04 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_account_public_access_block" "block_all_public" {
   block_public_acls   = true
   block_public_policy = true
-  ignore_public_acls  = true
+  ignore_public_acls  = false
   restrict_public_buckets  = true
 }
